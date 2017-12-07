479643e308be8d4c8425eb2aca66b72ac51b717c
cloudnodes <antoine.dray@epita.fr>
2017-12-07 17:45:42 +0100

    Updated database generator

--- neuralnet/database/gen.sh
+++ neuralnet/database/gen.sh
@@ -8,7 +8,7 @@ NC='\033[0m' # No Color
 
 if [ "$#" -eq 1 ]; then
     if [ $1 == "-gen" ]; then
-        listFonts="Arial Helvetica"
+        listFonts="Arial Georgia Verdana"
         listMaj="A B C D E F G H I J K L M N O P Q R S T U V W X Y Z"
         listMin="q w e r t y u i o p a s d f g h j k l z x c v b n m"
         listNum="0 1 2 3 4 5 6 7 8 9"
@@ -17,21 +17,21 @@ if [ "$#" -eq 1 ]; then
             for c in $listMaj
             do
                 convert -size 32x32     -gravity center  \
-                        -font Arial     -pointsize 32       label:${c} \
+                        -font ${f}     -pointsize 32       label:${c} \
                         ${c}_maj_${f}.jpg
             done
             echo "[${GREEN}SUCCESS${NC}] Generating ${f} Uppercases"
             for c in $listMin
             do
                 convert -size 32x32     -gravity center  \
-                        -font Arial     -pointsize 32       label:${c} \
+                        -font ${f}     -pointsize 32       label:${c} \
                         ${c}_min_${f}.jpg
             done
             echo "[${GREEN}SUCCESS${NC}] Generating ${f} Lowercases"
             for c in $listNum
             do
                 convert -size 32x32     -gravity center  \
-                        -font Arial     -pointsize 32       label:${c} \
+                        -font ${f}     -pointsize 32       label:${c} \
                         ${c}_${f}.jpg
             done
             echo "[${GREEN}SUCCESS${NC}] Generating ${f} numbers"
