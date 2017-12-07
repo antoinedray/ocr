b920327d83076358ec89d0a4aaee09adb4dd7e6e
cloudnodes <antoine.dray@epita.fr>
2017-12-07 18:47:00 +0100

    Upgraded database generator

--- neuralnet/database/gen.sh
+++ neuralnet/database/gen.sh
@@ -6,35 +6,40 @@ set -e
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
+FONTS='fonts/*'
+
 if [ "$#" -eq 1 ]; then
     if [ $1 == "-gen" ]; then
         listFonts="Arial Georgia Verdana"
         listMaj="A B C D E F G H I J K L M N O P Q R S T U V W X Y Z"
         listMin="q w e r t y u i o p a s d f g h j k l z x c v b n m"
         listNum="0 1 2 3 4 5 6 7 8 9"
-        for f in $listFonts
+        for f in $FONTS
         do
+            # We extract from the ttf file the name of the font
+            ff=${f%.*} # Remove extension
+            ff=${ff##*/} # Remove path to extension
             for c in $listMaj
             do
                 convert -size 32x32     -gravity center  \
-                        -font ${f}     -pointsize 32       label:${c} \
-                        ${c}_maj_${f}.jpg
+                        -font ${f}     -pointsize 25       label:${c} \
+                        ${c}_maj_${ff}.jpg
             done
-            echo "[${GREEN}SUCCESS${NC}] Generating ${f} Uppercases"
+            echo "[${GREEN}SUCCESS${NC}] Generating ${ff} Uppercases"
             for c in $listMin
             do
                 convert -size 32x32     -gravity center  \
-                        -font ${f}     -pointsize 32       label:${c} \
-                        ${c}_min_${f}.jpg
+                        -font ${f}     -pointsize 25       label:${c} \
+                        ${c}_min_${ff}.jpg
             done
-            echo "[${GREEN}SUCCESS${NC}] Generating ${f} Lowercases"
+            echo "[${GREEN}SUCCESS${NC}] Generating ${ff} Lowercases"
             for c in $listNum
             do
                 convert -size 32x32     -gravity center  \
-                        -font ${f}     -pointsize 32       label:${c} \
-                        ${c}_${f}.jpg
+                        -font ${f}     -pointsize 25       label:${c} \
+                        ${c}_${ff}.jpg
             done
-            echo "[${GREEN}SUCCESS${NC}] Generating ${f} numbers"
+            echo "[${GREEN}SUCCESS${NC}] Generating ${ff} numbers"
         done
     elif [ $1 == "-clean" ]; then
         rm -f *.jpg
