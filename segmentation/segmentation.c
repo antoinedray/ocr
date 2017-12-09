a0b374f2d0fef369e22cf0a32e819094316038cd
Moxyze <celian.gossec@epita.fr>
2017-12-09 04:54:42 +0100

    Character boxing is now DONE. Still need to use the letter struct to use it.

--- segmentation/segmentation.c
+++ segmentation/segmentation.c
@@ -20,7 +20,7 @@ SDL_Surface* whole_segmentation(SDL_Surface* img)
   for (int x = 0; x < img->w; x++)
     columns[x] = -1;
   char_detection(img, lines_cleaned, columns);
-  return(Display_Character_Boxes(img, lines_cleaned,columns)/*text_blocks(img, 1, lines, columns)*/);
+  return(/*Display_Character_Boxes(img, lines_cleaned,columns)*/text_blocks(img, 1, lines_cleaned, columns));
 }
 
 
@@ -127,9 +127,7 @@ void char_detection(SDL_Surface* img, int list[], int res[])
   for (int i = 0; i < img->h; i++) //Goes through the line list
   {
     if (list[i] == 1) //We found the top of a char line
-    {
       top_of_char_line_index = i;
-    }
     if (list[i] == 2)
     {
       for (int x = 0; x < img->w; x++)
@@ -153,10 +151,13 @@ void char_detection(SDL_Surface* img, int list[], int res[])
           // Warnx("column_list_index is :%d\n",column_list_index);
           letter = 0;
           res[column_list_index] = x;
-          column_list_index++;
+          //res[column_list_index + 1] = -1;
+          column_list_index += 1;//2;
         }
         black = 0;
       }
+      res[column_list_index] = -42; //EOL char
+      column_list_index++;
     }
   }
 }
@@ -209,22 +210,22 @@ SDL_Surface* Display_Character_Boxes(SDL_Surface* img, int startlines[],
 }
 
 
-/*
 
-   SDL_Surface* text_blocks(SDL_Surface* img, int scale, int lines[], int cols[])
- *
+
+SDL_Surface* text_blocks(SDL_Surface* img, int scale, int lines[], int cols[])
+/*
  ** Displays text blocks, depending on scale.
  ** 1: boxes around each letter
  ** 2: boxes around each word
  ** 3: boxes around each paragraph
- *
- {
- if (scale == 1)
- return box_letters(img, lines, cols);
- else
- return img;
- }
  */
+{
+  if (scale == 1)
+    return box_letters(img, lines, cols);
+  else
+    return img;
+}
+
 
 SDL_Surface* box_letters(SDL_Surface* img, int lines[], int cols[])
 { //Boxes every letter of the text
@@ -233,13 +234,13 @@ SDL_Surface* box_letters(SDL_Surface* img, int lines[], int cols[])
   int index = 0;
   Uint8 pxlcolor;
   Uint32 pxl;
-
   for (y = 0; y < img->h; y++)
   {
     if (lines[y] == 1)
     {
+  //    draw_line(img, 0, img->w, y);
       tmp = index;
-      for (; cols[tmp] < cols[tmp + 2];)
+      for (; cols[tmp] != -42; )
       {
         int keep_checking = 1;
         for (int tmp_y = y; keep_checking; tmp_y++)
@@ -261,7 +262,8 @@ SDL_Surface* box_letters(SDL_Surface* img, int lines[], int cols[])
     }
     if (lines[y] == 2)
     {
-      for (;cols[index] < cols[index + 2];)
+//      draw_line(img, 0, img->w, y);
+      for (; cols[index] != -42; )
       {
         int stop_checking = 0;
         for (int tmp_y = y; stop_checking != 1; tmp_y--)
@@ -272,7 +274,7 @@ SDL_Surface* box_letters(SDL_Surface* img, int lines[], int cols[])
             SDL_GetRGB(pxl, img->format, &pxlcolor, &pxlcolor, &pxlcolor);
             if (pxlcolor == 0)
             {
-              draw_line(img, cols[index], cols[index+1], tmp_y + 1);
+              draw_line(img, cols[index], cols[index+1], tmp_y);
               stop_checking = 1;
               index += 2;
               break;
@@ -280,23 +282,24 @@ SDL_Surface* box_letters(SDL_Surface* img, int lines[], int cols[])
           }
         }
       }
+      index++;
     }
   }
-  return (img);
+  return img;
 }
 
 /*
  **  name: box_fit
  **  description: take the box containing the letter and fit it in a optimal box
  */
-SDL_Surface* box_fit(SDL_Surface* img) {
+/*SDL_Surface* box_fit(SDL_Surface* img) {
   // Remove bottom
   for(int h = 0; h < img->h; h++) {
     //
   }
 }
 return (img);
-}
+}*/
 
 SDL_Surface* draw_line(SDL_Surface* img, int start_x, int end_x, int y)
 { //Draws a line on img, at height y, between points s_x and end_x
