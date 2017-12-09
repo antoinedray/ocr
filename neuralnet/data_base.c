99e717685711080f38f512d4f5fd9afd80824a95
cloudnodes <antoine.dray@epita.fr>
2017-12-09 01:43:30 +0100

    Updated data_base.c

--- neuralnet/data_base.c
+++ neuralnet/data_base.c
@@ -6,7 +6,7 @@ static struct letter_bin *IMGtoBIN(SDL_Surface* img)
   Uint8 r, g, b;
   int height = img->h;
     int width = img->w;
-    double *mat = calloc(width*height, sizeof(double));
+    double *mat = calloc(width * height, sizeof(double));
     for(int h = 0; height > h; h++)
     {
         for (int w = 0; width > w; w++)
@@ -18,20 +18,23 @@ static struct letter_bin *IMGtoBIN(SDL_Surface* img)
                 mat[w + h * width] = 1.0;
         }
     }
-    struct letter_bin *bin = malloc(sizeof(struct BIN_Matrix));
+    struct letter_bin *bin = malloc(sizeof(struct letter_bin));
     bin->inputs = mat;
     bin->len = height*width;
     return bin;
 }
 
 /*
+        *FOR STAN*
+
+    What is the use of the structure Bin matrix in your header ?
+
      *FOR ANTOINE*
 
 size_letter = height*width
 before hand, DB must be 32*32.
 the mat will have the following struct: nb_fonts*nb_characters(62 by default) = lines,
-32*32 = columns
-order : aaaaaabbbbbb....AAAAABBBBBB...11111112222222...
+order : same font -> maj / min / num
 */
 
 double **get_database_in(size_t nb_fonts,size_t nb_characters) {
@@ -43,26 +46,17 @@ double **get_database_in(size_t nb_fonts,size_t nb_characters) {
     while((linelen = getline(&line, &linecap, fp)) > 0) {
         letter_sdl = load_image(line);
         struct letter_bin *letter = IMGtoBIN(letter_sdl);
-        // More code
+        // Plug the struct letter_bin to the program
     }
     free(letter_sdl);
-
-
-    double **inputs = malloc(sizeof(double*)*nb_fonts*nb_characters);
-    for(size_t i = 0; i < nb_fonts*nb_characters; i++){
-        //get the letter_img
-        SDL_Surface* letter_img = load_image;
-        struct letter_bin *letter = IMGtoBIN(letter_img);
-        inputs[i] = l->inputs;
-    }
 }
 
 double **get_database_out(size_t nb_characters){
     double **outputs = malloc(sizeof(double*)*nb_characters);
     for(size_t i = 0; i < nb_characters;i++){
-        outputs[i] = calloc(sizeof(double)*nb_characters);
+        outputs[i] = calloc(nb_characters, sizeof(double));
         outputs[i][i] = 1.0;
     }
-    return output;
+    return outputs;
 }
 
