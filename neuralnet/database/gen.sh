1ec9df6931e2418922a86ec15906aca3d23ad5ae
cloudnodes <antoine.dray@epita.fr>
2017-12-10 13:32:42 +0100

    Added some special characters to the database

--- neuralnet/database/gen.sh
+++ neuralnet/database/gen.sh
@@ -17,6 +17,8 @@ listFonts="Arial Georgia Verdana"
 listMaj="A B C D E F G H I J K L M N O P Q R S T U V W X Y Z"
 listMin="a b c d e f g h i j k l m n o p q r s t u v w x y z"
 listNum="0 1 2 3 4 5 6 7 8 9"
+listChars=", . ! ? : ; ' \" ( ) $ -"
+
 
 for f in $FONTS
 do
@@ -49,5 +51,15 @@ do
                 ${c}_num_${ff}${FORMAT}
         echo "${c}_num_${ff}${FORMAT}" >> $DATAFILE
     done
-    echo "[${GREEN}SUCCESS${NC}] Generating ${ff} numbers"
+    echo "[${GREEN}SUCCESS${NC}] Generating ${ff} Numbers"
+    i=0
+    for c in $listChars
+    do
+        convert -size ${SIZE}     -gravity center  \
+                -font ${f}        -pointsize 25       label:${c} \
+                ${i}_char_${ff}${FORMAT}
+        echo "${i}_char_${ff}${FORMAT}" >> $DATAFILE
+        i=$(expr $i + 1)
+    done
+    echo "[${GREEN}SUCCESS${NC}] Generating ${ff} Special characters"
 done
