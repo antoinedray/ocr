4826a88d4d70ba5d4168aa03588b2a4c549d9ced
cloudnodes <antoine.dray@epita.fr>
2017-12-11 00:04:14 +0100

    modified gen.sh to remove special characters

--- neuralnet/database/gen.sh
+++ neuralnet/database/gen.sh
@@ -16,7 +16,7 @@ touch $DATAFILE
 listMaj="A B C D E F G H I J K L M N O P Q R S T U V W X Y Z"
 listMin="a b c d e f g h i j k l m n o p q r s t u v w x y z"
 listNum="0 1 2 3 4 5 6 7 8 9"
-listChars=", . ! ? : ; ' \" ( ) $ - &"
+listChars=", . ! ?"
 
 
 for f in $FONTS
